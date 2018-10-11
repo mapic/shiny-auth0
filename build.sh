e48f2c5b31f9e6c3272099679f2a6a906794a3e6
knutole <knutole@mapic.io>
2018-10-11 22:39:16 +0000

    verify build

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# build
 docker build -t mapic/shiny-auth0 .
 
+# verify
+if [ "$?" -gt "0" ]; then
+    echo "Build errored."
+    exit 1
+fi
+
+# push
 docker push mapic/shiny-auth0:latest
