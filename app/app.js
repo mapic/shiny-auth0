7daf17aea2744f8a0441d69442d08cecc6814de6
knutole <knutole@mapic.io>
2018-10-24 13:22:28 +0000

    improve err handling

--- app/app.js
+++ app/app.js
@@ -88,9 +88,11 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
+    var msg = req.query.error_description;
+    console.log('msg:', msg);
     res.render('error', {
-      message: err.message,
-      error: err
+      message: msg,
+      error: {}
     });
   });
 }
@@ -99,6 +101,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
+  console.log('err:', err);
   res.render('error', {
     message: err.message,
     error: {}
